9c5cc51c078103ab9e588ff967f9c4a5ca236f69
anuwataravis <anuwat.avis@gmail.com>
2020-08-23 13:07:46 +0700

    add app

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "This is my homepage 099"
+
+
+@app.route('/hello')
+def hello():
+    return {"message": "Auwat api welcome"}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
